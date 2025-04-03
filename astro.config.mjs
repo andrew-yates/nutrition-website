25927d3178226cdb548934de24bc47dadecf26d0
andrew-yates <andrew@laneyates.com>
2025-04-03 00:36:33 -0700

    once more

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  site: 'https://andrew-yates.github.io/nutrition-website',
-  base: '/nutrition-website',
+  site: 'https://andrew-yates.github.io/nutrition-website/dist',
+  base: '/nutrition-website/dist',
 });
